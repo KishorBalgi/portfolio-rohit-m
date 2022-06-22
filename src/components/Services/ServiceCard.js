301eab15aaf379e77cf23ffef0b0d56783852302
Kishor Balgi <kishorbalgi@gmail.com>
2022-06-22 22:43:45 +0530

    Minor bug fixes

--- src/components/Services/ServiceCard.js
+++ src/components/Services/ServiceCard.js
@@ -5,7 +5,7 @@ export default function ServiceCard({ img, title, desc }) {
   return (
     <Row className="justify-content-center service-card">
       <Col lg={5} className="service-card-img">
-        <img src={img} />
+        <img src={img} alt={title} />
       </Col>
       <Col lg={7} className="service-card-details" align="left">
         <h2>{title}</h2>
