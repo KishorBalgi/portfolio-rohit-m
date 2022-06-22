b7d821109de5e5845e7cd4e90ffaf7165d8ff7ec
Kishor Balgi <kishorbalgi@gmail.com>
2022-06-22 23:21:42 +0530

    Minor responsiveness fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,19 +9,19 @@ function Contact() {
           <span className="purple">CONTACT</span>
         </h1>
         <Row className="contact-details">
-          <Col sm={6}>
+          <Col xs={12} sm={6} md={4}>
             <div>
               <h3>Phone</h3>
               <p className="purple">+91 9876543210</p>
             </div>
           </Col>
-          <Col sm={6}>
+          <Col xs={12} sm={6} md={4}>
             <div>
               <h3>Skype Id</h3>
               <p className="purple">johndoe@john.com</p>
             </div>
           </Col>
-          <Col sm={12}>
+          <Col xs={12} sm={12} md={4}>
             <div>
               <h3>Email</h3>
               <p className="purple">johndoe@john.com</p>
